034f435c041c8cf45de8e6b04d46f59b624a4e7b
Gaurav0818 <Gaurav.ps.0818@gmail.com>
2023-07-06 10:14:02 +0530

    Added Singleton Template

--- Assets/Scripts/Mangers/TimelineManager.cs
+++ Assets/Scripts/Mangers/TimelineManager.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimelineManager : MonoBehaviour
+public class TimelineManager : Singleton<TimelineManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
