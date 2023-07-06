d8d6954370d0cdbe28ddc1d43f912919b824b9a5
Gaurav0818 <Gaurav.ps.0818@gmail.com>
2023-07-06 18:40:03 +0530

    Weekend and WeekDay logic

--- Assets/Scripts/Mangers/TimelineManager.cs
+++ Assets/Scripts/Mangers/TimelineManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TimelineManager : Singleton<TimelineManager>
@@ -10,14 +12,58 @@ public class TimelineManager : Singleton<TimelineManager>
         WeekDay,
         WeekEnd
     }
+    
+    [System.Serializable]
+    public struct ScenarioData
+    {
+        public Scenario scenario;
+        public int StartTime;
+    }
 
     [System.Serializable]
     public struct DayEntry
     {
+        public int dayNumber;
         public DayType typeOfDay;
-        public List<Scenario> scenarioList;
+        public List<ScenarioData> scenarioList;
     }
-
+    
+    [System.Serializable]
+    public enum GameMode
+    {
+        WeekBased,
+        MonthBased
+    }
+    
+    public GameMode gameMode;
     public List<DayEntry> days;
 
+    private void Awake()
+    {
+        if(gameMode == GameMode.WeekBased)
+            InitaizeDays(7);
+        else
+            InitaizeDays(30);
+    }
+
+    
+    private void InitaizeDays(int dayCount)
+    {
+        for (int i = 1; i <= dayCount; i++)
+        {
+            DayEntry day = new DayEntry();
+            day.dayNumber = i;
+            
+            // statement to find 7n or 7n -1 day of the week
+            // i % 7 will day of the week from 1 to 7
+            // 7 - i % 7 will be smaller then or equal to 1 for weekends and greater then 1 for weekdays 
+            
+            if(7 - i % 7 <= 1)
+                day.typeOfDay = DayType.WeekEnd;
+            else
+                day.typeOfDay = DayType.WeekDay;
+            
+            days.Add(day);
+        }
+    }
 }
